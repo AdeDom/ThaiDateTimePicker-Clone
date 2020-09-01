3317d97bdd8a8ba4dbc4d5eead8557d1724884ef
AdeDom <pathiphonyw@gmail.com>
2020-09-01 09:45:53 +0700

    convert java to kotlin class AccessibleLinearLayout

---
+++ library/src/main/java/com/adedom/library/util/AccessibleLinearLayout.kt
@@ -0,0 +1,24 @@
+package com.adedom.library.util
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.accessibility.AccessibilityEvent
+import android.view.accessibility.AccessibilityNodeInfo
+import android.widget.Button
+import android.widget.LinearLayout
+
+class AccessibleLinearLayout(
+    context: Context?, attrs: AttributeSet?
+) : LinearLayout(context, attrs) {
+
+    override fun onInitializeAccessibilityEvent(event: AccessibilityEvent?) {
+        super.onInitializeAccessibilityEvent(event)
+        event?.className = Button::class.java.name
+    }
+
+    override fun onInitializeAccessibilityNodeInfo(info: AccessibilityNodeInfo?) {
+        super.onInitializeAccessibilityNodeInfo(info)
+        info?.className = Button::class.java.name
+    }
+
+}
