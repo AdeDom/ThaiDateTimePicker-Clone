fd5de35234f3c71dbf597eb034feb1d9b041bac8
AdeDom <pathiphonyw@gmail.com>
2020-09-01 11:30:53 +0700

    using android library calendar

--- app/src/main/java/com/adedom/thaidatetimepicker_clone/MainActivity.kt
+++ app/src/main/java/com/adedom/thaidatetimepicker_clone/MainActivity.kt
@@ -3,7 +3,7 @@ package com.adedom.thaidatetimepicker_clone
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.adedom.library.datev2.chocoCardCalendarDialog
+import com.adedom.calendar.date.chocoCardCalendarDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,9 +12,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         textView.setOnClickListener {
-            chocoCardCalendarDialog { _, year, monthOfYear, dayOfMonth ->
-                val date = "$dayOfMonth/${monthOfYear.plus(1)}/$year"
-                Toast.makeText(baseContext, date, Toast.LENGTH_SHORT).show()
+            chocoCardCalendarDialog {
+                Toast.makeText(baseContext, it, Toast.LENGTH_SHORT).show()
             }
         }
     }
--- calendar/src/main/java/com/adedom/calendar/date/DatePickerDialogExt.kt
+++ calendar/src/main/java/com/adedom/calendar/date/DatePickerDialogExt.kt
@@ -3,10 +3,17 @@ package com.adedom.calendar.date
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
-fun AppCompatActivity.chocoCardCalendarDialog(callBack: DatePickerDialog.OnDateSetListener) {
+fun AppCompatActivity.chocoCardCalendarDialog(work: (String) -> Unit) {
     val calendar = Calendar.getInstance()
     DatePickerDialog.newInstance(
-        callBack,
+        object : DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(
+                view: DatePickerDialog,
+                year: Int,
+                monthOfYear: Int,
+                dayOfMonth: Int
+            ) = work.invoke("$dayOfMonth/${monthOfYear.plus(1)}/$year")
+        },
         calendar.get(Calendar.YEAR),
         calendar.get(Calendar.MONTH),
         calendar.get(Calendar.DAY_OF_MONTH)
